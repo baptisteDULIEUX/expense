85d7bdc7e0816518ae56036f7f410584d688a3de
baptisteDULIEUX <baptiste.dulieux25420@gmail.com>
2025-01-28 09:20:49 +0100

    Initial Commit

---
+++ expense/expenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseApp.swift
+//  expense
+//
+//  Created by dulieux baptiste on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct expenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
