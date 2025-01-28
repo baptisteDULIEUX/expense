67874c9296ae0e8ee068989cd7a9676d37f5f02c
baptisteDULIEUX <baptiste.dulieux25420@gmail.com>
2025-01-28 09:49:36 +0100

    nouveau tp

--- expense/Models/History.swift
+++ expense/Models/History.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+
+struct Hystory: Identifiable {
+    var id = UUID()
+    var title: String
+    var value: Double
+    var type: String
+    
+    static var testData = [
+        Hystory(title: "Le bon de jb", value: 140.5, type: "Perso"),
+        Hystory(title: "Shaco", value: 150.0, type: "Professionnel"),
+        
+    ]
+}
